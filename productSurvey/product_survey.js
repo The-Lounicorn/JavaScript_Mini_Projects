f64764cdca400cec67a86ecb090b84cab59707bd
The_Lounicorn <m.lou.landy@gmail.com>
2025-08-13 13:49:29 -0400

    add user experience html and logic

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function  submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById("userExperience").value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -13,7 +14,9 @@ function  submitFeedback() {
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
-    document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userReview').innerHTML = feedback;
+    document.getElementById('userExperienceDisplay').innerHTML = experience;
+
 
     document.getElementById('userInfo').style.display = 'block';
 
