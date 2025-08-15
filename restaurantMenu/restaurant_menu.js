fd7377fa6d0a970949eb6791afa0c6d7109a5f88
The_Lounicorn <m.lou.landy@gmail.com>
2025-08-15 10:44:59 -0400

    included comments

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -2,14 +2,17 @@ const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $15', 'Oatmeal - $9', 
 const mainCourseMenu = ['Steak - $40', 'Chicken - $22', 'Pasta - $25', 'Burger - $20', 'Salmon - $32'];
 const dessertMenu = ['Cake - $10', 'Ice Cream - $8', 'Pudding - $9', 'Fruit Salad - $7'];
 
+//utilizing map method
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
+//utilizing forEach method
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
+//utilizing for loop
 let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
